c4e98de4c75c63d18bda3b2a17e7a6beec69d45f
Will Couture <wcouture17@gmail.com>
2026-04-21 00:17:35 +0000

    Update file index.js

--- site-data/index.js
+++ site-data/index.js
@@ -427,7 +427,7 @@ app.post("/file-upload", upload.single('file'), (req, res) => {
 		</span><br/>
 	</div>
 	`;
-	send_message("wcouture17@gmail.com", "File Upload", html);
+	send_message(message_recipient, "File Upload", html);
 	res.send(JSON.stringify({"status": "success"}));
 })
 
